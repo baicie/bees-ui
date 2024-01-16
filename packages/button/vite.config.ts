ff2367d89d45506ee77ebd19a301c4eb1006763f
刘志伟 <2055572879@qq.com>
2024-01-16 15:43:25 +0800

    feat: build

---
+++ packages/button/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import solid from 'vite-plugin-solid'
+
+export default defineConfig({
+  plugins: [solid()],
+  build: {
+    rollupOptions:{
+      input: 'src/index.tsx'
+    }
+  }
+})
