2c971108529973430b79bcf38cefdd892348e9d7
刘志伟 <2055572879@qq.com>
2024-01-16 15:43:25 +0800

    feat: build

--- playground/svelte/src/main.ts
+++
@@ -1,9 +0,0 @@
-import './app.css'
-import App from './App.svelte'
-import "carbon-components-svelte/css/all.css";
-
-const app = new App({
-  target: document.getElementById('app'),
-})
-
-export default app
