6ff2b0cb64b521b94b8c85ab85c6754cb0639961
刘志伟 <2055572879@qq.com>
2024-09-05 15:11:15 +0800

    fix: build

--- internal/build/src/index.ts
+++ internal/build/src/index.ts
@@ -3,6 +3,7 @@ import cac from 'cac';
 
 import { build, watchFuc } from './build';
 import { dts } from './dts';
+import { DEFAULT } from './ustils';
 
 const cli = cac('bee');
 cli.command('[root]', 'Build the project').action(() => {
@@ -13,7 +14,7 @@ cli
   .command('build', 'build mode')
   .option('-w, --watch', 'Production mode')
   .option('-c, --config', 'Production mode')
-  .option('-i, --input <input...>', 'input path', { type: [] })
+  .option('-i, --input <input...>', 'input path', { type: [], default: DEFAULT })
   .option('-m, --minify', 'output path')
   .option('-f, --full', 'output path')
   .option('-s, --sourcemap', 'output path')
