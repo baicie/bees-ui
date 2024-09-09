bcf3fe86dfc2f61106e7de926d15d49f03bf17af
刘志伟 <2055572879@qq.com>
2024-09-09 20:25:25 +0800

    switch

---
+++ packages/sc-switch/src/index.tsx
@@ -0,0 +1,85 @@
+import classNames from 'clsx';
+import { createMemo, createSignal, mergeProps, splitProps } from 'solid-js';
+import type { JSX } from 'solid-js';
+
+export type SwitchChangeEventHandler = (
+  checked: boolean,
+  event: MouseEvent | KeyboardEvent,
+) => void;
+
+export type SwitchClickEventHandler = SwitchChangeEventHandler;
+
+interface SwitchProps
+  extends Omit<JSX.ButtonHTMLAttributes<HTMLButtonElement>, 'onClick' | 'onChange'> {
+  className?: string;
+  prefixCls?: string;
+  disabled?: boolean;
+  checkedChildren?: JSX.Element;
+  unCheckedChildren?: JSX.Element;
+  onChange?: SwitchChangeEventHandler;
+  onKeyDown?: (event: KeyboardEvent) => void;
+  onClick?: SwitchClickEventHandler;
+  tabIndex?: number;
+  checked?: boolean;
+  defaultChecked?: boolean;
+  loadingIcon?: JSX.Element;
+  style?: JSX.CSSProperties;
+  title?: string;
+}
+
+const Switch = (props: SwitchProps) => {
+  props = mergeProps({ prefixCls: 'rc-switch', defaultChecked: false }, props);
+  const [_, rest] = splitProps(props, ['onChange']);
+  const [innerChecked, setInnerChecked] = createSignal(props.defaultChecked);
+  console.log('innerChecked', props.disabled);
+
+  const triggerChange = (newChecked: boolean, event: MouseEvent | KeyboardEvent) => {
+    if (!props.disabled) {
+      setInnerChecked(newChecked);
+      props.onChange?.(newChecked, event);
+    }
+  };
+
+  const onInternalKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'ArrowLeft') {
+      triggerChange(false, e);
+    } else if (e.key === 'ArrowRight') {
+      triggerChange(true, e);
+    }
+    props.onKeyDown?.(e);
+  };
+
+  const onInternalClick = (e: MouseEvent) => {
+    const ret = !innerChecked();
+    triggerChange(ret, e);
+    props.onClick?.(ret, e);
+  };
+
+  const switchClassName = createMemo(() =>
+    classNames(props.prefixCls, props.className, {
+      [`${props.prefixCls}-checked`]: innerChecked(),
+      [`${props.prefixCls}-disabled`]: props.disabled,
+    }),
+  );
+
+  return (
+    <button
+      {...rest}
+      type="button"
+      role="switch"
+      aria-checked={innerChecked()}
+      disabled={props.disabled}
+      class={switchClassName()}
+      onKeyDown={onInternalKeyDown}
+      onClick={onInternalClick}
+    >
+      {props.loadingIcon}
+      <span class={`${props.prefixCls}-inner`}>
+        <span class={`${props.prefixCls}-inner-checked`}>{props.checkedChildren}</span>
+        <span class={`${props.prefixCls}-inner-unchecked`}>{props.unCheckedChildren}</span>
+      </span>
+    </button>
+  );
+};
+
+export default Switch;
--- playground/sc-comps/src/App.tsx
+++ playground/sc-comps/src/App.tsx
@@ -1,11 +1,24 @@
 import Input from '@bees-ui/sc-input';
+import type { SwitchChangeEventHandler } from '@bees-ui/sc-switch';
+import Switch from '@bees-ui/sc-switch';
 
+import './index.less';
 import './App.css';
 
-import { onMount } from 'solid-js';
+import { createSignal, onMount } from 'solid-js';
+
+const onChange: SwitchChangeEventHandler = (value, event) => {
+  console.log(`switch checked: ${value}`, event);
+};
 
 function App() {
+  const [disabled, setDisabled] = createSignal(false);
   let ref: HTMLInputElement | null = null;
+
+  const toggle = () => {
+    setDisabled((prev) => !prev);
+    console.log('disabled', disabled());
+  };
 
   onMount(() => {
     console.log(ref?.focus());
@@ -19,7 +32,18 @@ function App() {
         allowClear
         placeholder="sc-input"
       ></Input>
-      <div>div</div>
+
+      <Switch
+        onChange={onChange}
+        disabled={disabled()}
+        checkedChildren="开"
+        unCheckedChildren="关"
+      />
+      <div style={{ 'margin-left': '20px' }}>
+        <button type="button" onClick={toggle}>
+          toggle disabled
+        </button>
+      </div>
     </>
   );
 }
